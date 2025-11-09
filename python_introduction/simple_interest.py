93e78028eab0c3ff78fc51c141be34b426520261
Amir Abdu <amirabdu.com@gmail.com>
2025-11-09 08:56:40 +0300

    Add rectangle area calculation and improve simple interest output

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,4 +4,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-print("The simple interest is: ", principal * rate * time)
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
