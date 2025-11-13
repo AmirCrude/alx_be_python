2b7ffdddb77c7b6c30b7b42a01e898d4407926e8
Amir Abdu <amirabdu.com@gmail.com>
2025-11-13 09:22:49 +0300

    Add pattern drawing script to generate asterisks based on user-defined size

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+﻿
+size = int(input("Enter the size of the pattern: "))
+count = 1
+
+if size <= 0:
+    print("Please enter a positive integer.")
+else:
+    while count <= size:
+        for i in range(1, size + 1):
+            print("*", end="")
+        print()
+        count += 1
