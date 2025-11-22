8cdfba660507d7edacd67319a013dddd1bbe823b
Amir Abdu <amirabdu.com@gmail.com>
2025-11-22 16:49:23 +0300

    Update the perform_operation function to operate with elif instead of match-case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,35 +1,13 @@
-﻿
-def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return add(num1, num2)
-        case 'subtract':
-            return subtract(num1, num2)
-        case 'multiply':
-            return multiply(num1, num2)
-        case 'divide':
-            if num2 == 0:
-                return "Can not divide by 0."
-            else:
-                return divide(num1, num2)
-        case _:
-            print(f"Unknown operation: {operation}")
-
-def add(num1, num2):
-    result = num1 + num2
-    return result
-
-def subtract(num1, num2):
-    result = num1 - num2
-    return result
-
-def multiply(num1, num2):
-    result = num1 * num2
-    return result
-
-def divide(num1, num2):
-    if num2 != 0:
-        result = num1 / num2
-        return result 
+﻿def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by 0."
+        return num1 / num2
     else:
-        print("Can not divide by 0.")
+        return "Unknown operation"
