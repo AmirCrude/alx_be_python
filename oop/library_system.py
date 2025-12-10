d26d4e4f39ecd72c7881412823327615fcb3ed68
Amir Abdu <amirabdu.com@gmail.com>
2025-12-10 19:17:20 +0300

    Add Library, Book, EBook, and PrintBook classes with functionality to manage a library system

---
+++ oop/library_system.py
@@ -0,0 +1,43 @@
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def get_info(self):
+        return f"Book: '{self.title}' by {self.author}"
+    
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def get_info(self):
+        return  f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+    
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def get_info(self):
+        return f"Print Book: {self.title} by {self.author}, Pages: {self.page_count}"
+    
+class Library:
+    def __init__(self):
+        self.books = []
+    
+    def add_book(self, book):
+        if isinstance(book, (Book, EBook, PrintBook)):
+            self.books.append(book)
+            print(f"Added to library: {book.title}")
+        else:
+            print("Error: Can only add Book objects to the library")
+    
+    def list_books(self):
+        print("\nLibrary Collection")
+        if not self.books:
+            print("The library is empty.")
+        else:
+            for book in self.books:
+                print(book.get_info())
